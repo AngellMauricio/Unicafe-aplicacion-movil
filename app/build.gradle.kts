e0ed4545b7f195c21d50afb24f969026149c376c
Victor <20241002@uthh.edu.mx>
2025-11-24 07:37:32 -0600

    Implementar las librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,32 @@ dependencies {
 // Use a recent version
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+// Keep these (and ensure they use the latest version)
+    implementation("androidx.media3:media3-exoplayer:1.8.0")
+    implementation("androidx.media3:media3-ui:1.8.0")
+    implementation("androidx.media3:media3-exoplayer-dash:1.8.0")
+    implementation("androidx.media3:media3-extractor:1.8.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
+
+    // PRUEBAS DE INSTRUMENTACIÓN (androidTestImplementation)
+    // Framework de pruebas principal
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:rules:1.7.0")
+
+    // Espresso para interactuar con la UI
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // MockWebServer: Para simular el servidor de la API
+    androidTestImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
+
+    // Si usas Hilt para inyección de dependencias
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.57.2")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0") // Ayuda con sintaxis Kotlin
+    testImplementation(kotlin("test"))
 }
